32bccbab468732cace341c829df64c62bf21f091
iris_ <iris_starry_@naver.com>
2023-04-11 00:15:39 +0900

    Performance evaluation for the first semester

---
+++ src/test/Max.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.Scanner;
+
+public class Max {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num[] = new int[10];
+		int bic=0;
+		for(int i=0; i<num.length; i++) {
+			System.out.print((i+1)+"번째 수 : ");
+			num[i]=sc.nextInt();
+		}
+		
+		bic=num[0];
+		for(int i=0; i<num.length; i++) {
+			if(bic<num[i]) {
+				bic=num[i];
+			}
+		}
+		
+		System.out.println("가장 큰 수 : "+bic);
+	}
+
+}
